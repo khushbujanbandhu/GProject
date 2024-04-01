d30a675d2bac17903f48f268c06ec523187fbf20
khushbu janbandhu <khushbujanbandhu28@gmail.com>
2024-04-01 19:56:31 +0530

    added sop

--- MyGitPro/src/com/myg/Samp.java
+++ MyGitPro/src/com/myg/Samp.java
@@ -2,4 +2,11 @@ package com.myg;
 
 public class Samp {
 
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("value of i is>>" + i);
+		}
+		System.out.println("value of ii is");	
+	}
 }
